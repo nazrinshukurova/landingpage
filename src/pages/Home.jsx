dafc8075df3add9276517d40ca2caf7d331d1c47
nazrinshukurova <nezrinnshukurova23>
2025-02-28 01:09:29 +0400

    Add navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Section_3 from "../components/HMComponents/ThirdSection/Section_3";
 import Section_4 from "../components/HMComponents/FourthSection/Section_4";
 import Section_5 from "../components/HMComponents/FifthSection/Section_5";
 import Section_last_ from "../components/HMComponents/FinalSection/Section_last_";
+import Footer from "../shared/Footer/Footer";
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
       <Section_4 />
       <Section_5 />
       <Section_last_ />
+      <Footer />
     </div>
   );
 };
--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
+import Button from "../Button/Button";
+import facebook from "../../assets/svg/social/facebook.svg";
+import linkedin from "../../assets/svg/social/linkedin.svg";
+import twitter from "../../assets/svg/social/twitter.svg";
+import youtube from "../../assets/svg/social/youtube.svg";
+import instagram from "../../assets/svg/social/instagram.svg";
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="footer">
+      <footer>
+        <div className="copyright">©2023 Yourcompany</div>
+        <div className="logo-2">Landing</div>
+        <Button text="Purchase Now" />
+      </footer>
+      <div className="rectangle-box">
+        <div className="rectangle"></div>
+      </div>
+      <div className="footer-bottom">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <div className="social-links">
+          <div>
+            <img src={facebook}></img>
+          </div>
+          <div>
+            <img src={linkedin}></img>
+          </div>
+          <div>
+            <img src={twitter}></img>
+          </div>
+          <div>
+            <img src={youtube}></img>
+          </div>
+          <div>
+            <img src={instagram}></img>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
